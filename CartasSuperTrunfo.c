c4b5331b504135f2da9eb5a84e0187a0553ba09a
mininistrela <cassioerah@gmail.com>
2024-11-26 01:27:38 +0000

    Declaracao de variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,44 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    char estado[30];
+    char cidade[30];
+    char codigo[3];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;  
+    
+    printf("Digite o nome do estado: \n");
+    scanf("%s", &estado);
+    printf("Estado: %s\n", estado);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+    printf("Cidade: %s\n", cidade);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &codigo);
+    printf("Código: %s\n", codigo);
+
+    printf("Digite a população da cidade: \n");
+    scanf("%d", &populacao);
+    printf("População: %d\n", populacao);
+
+    printf("Digite a area da cidade: \n");
+    scanf("%f", &area);
+    printf("Área: %f\n", area);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pib);
+    printf("PIB: %f\n", pib);
+
+    printf("Digite o numero de pontos turisticos da cidade: \n");
+    scanf("%d", &pontos_turisticos);
+    printf("Pontos turísticos: %d\n", pontos_turisticos);
+
+    return 0;
+  
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
